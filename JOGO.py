fe7a0318ca2c6e0cd42f710e6196f1433e73f6d4
Lucas <lucas.levi.vaz@gmail.com>
2022-05-13 10:37:02 -0300

    Tudo que fizemos juntos até agora

--- JOGO.py
+++ JOGO.py
@@ -1,6 +1,8 @@
+from http.client import OK
 import random
 from Sorteio_países import sorteia_pais 
 from Base_países import normaliza
+from Está_na_lista import esta_na_lista
 
 dados = {
   "asia": {
@@ -3821,3 +3823,19 @@ dados = normaliza(dados)
 sorteio =  sorteia_pais(dados)
 
 print(sorteio)
+
+
+print('Um país foi escolhido, tente adivinhar!\n')
+pais = input('Qual o seu palpite?\n')
+
+lista_paises = []
+for pais in dados.keys():
+    lista_paises.append(pais)
+
+if pais in lista_paises:
+    if pais == sorteio:
+        print('Parabéns, você acertou!')
+    else:
+        print()
+else:
+    print('País desconhecido')
