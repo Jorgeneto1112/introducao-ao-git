a3369c7e1776de3b9f77951e813107a865afc20a
Lucas <lucas.levi.vaz@gmail.com>
2022-05-13 10:14:09 -0300

    Deixei o teste

---
+++ Errado.py
@@ -0,0 +1,10 @@
+import random
+from Sorteio_países import sorteia_pais 
+from Base_países import normaliza
+
+with open('DADOS.py') as arquivo:
+    dados = normaliza(arquivo)
+
+sorteio =  sorteia_pais(dados)
+
+print(sorteio)
